1260e4f0004d820e7e776bdae212d40f157320ce
yashsharma6391 <ys7097569@gmail.com>
2025-09-12 18:21:53 +0530

    Create NavLogo.jsx

---
+++ Hexicom/src/Component/Navbar/NavLogo.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './NavLogo.css'
+
+const NavLogo = () => {
+  return (
+    <div class="companylogo">
+      <div class="logo_img">
+        <img src="Loogo.png" alt="" />
+      </div>
+      <div class="companyname">
+        <div class="Letters">
+          <div class="leter">h</div>
+          <div class="leter">e</div>
+          <div class="leter">x</div>
+          <div class="leter">i</div>
+          <div class="leter">c</div>
+          <div class="leter"><img src="Loogo.png" alt="" /></div>
+          <div class="leter">m</div>
+        </div>
+        <div class="arrows">
+          <div class="arrow1"></div>
+          <div class="arrow2"></div>
+          <div class="arrow3"></div>
+          <div class="arrow4"></div>
+        </div>
+        
+        
+
+
+        
+      </div>
+       <div class="tagline">
+          A Integrated Bridging of Technology & Infrastructure”
+        </div>
+    </div>
+  )
+}
+
+export default NavLogo
