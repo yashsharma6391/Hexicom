8f9eeb3b490100a1f0daf5141ff9837cb3941178
yashsharma6391 <ys7097569@gmail.com>
2025-09-16 01:58:01 +0530

    Update About.jsx

--- Hexicom/src/Pages/About.jsx
+++ Hexicom/src/Pages/About.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "/About.css";
+import "./About.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Sidebar from "../Component/Sidebar/Sidebar";
