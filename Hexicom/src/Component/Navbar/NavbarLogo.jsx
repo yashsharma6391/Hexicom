e819647790e39fddc9332c1e2b2fdc66698107c8
yashsharma6391 <ys7097569@gmail.com>
2025-09-17 10:58:12 +0530

    Update NavbarLogo.jsx

--- Hexicom/src/Component/Navbar/NavbarLogo.jsx
+++ Hexicom/src/Component/Navbar/NavbarLogo.jsx
@@ -1,49 +1,49 @@
-import React from "react";
-import "./NavbarLogo.css";
-const NavbarLogo = () => {
-  return (
-    <div className="company_logo">
-      <div className="logo">
-        <div className="vertical1"></div>
-        <div className="vertical2"></div>
-        <div className="vertical3">&#8734;</div>
-        <div className="vertical1"></div>
-        <div className="vertical1"></div>
-        <div className="vertical1"></div>
-        <div className="vertical7"></div>
-        <div className="vertical8"></div>
-        <div className="vertical9"></div>
-        <div className="vertical1"></div>
-        <div className="vertical1"></div>
-        <div className="vertical12"></div>
-        <div className="vertical13"></div>
-        <div className="vertical14"></div>
-        <div className="vertical1"></div>
-        <div className="vertical1"></div>
-        <div className="vertical17"></div>
-        <div className="vertical18"></div>
-        <div className="vertical19"></div>
-        <div className="vertical1"></div>
-        <div className="vertical1"></div>
-        <div className="vertical1"></div>
-        <div className="vertical1"></div>
-        <div className="vertical24"></div>
-        <div className="vertical1"></div>
-      </div>
-      <div className="title">
-        exicom
-        <div className="arrows">
-          <div className="arrow1"></div>
-          <div className="arrow2"></div>
-          <div className="arrow3"></div>
-          <div className="arrow4"></div>
-        </div>
-      </div>
-      <div className="tagline">
-        A Integrated Bridging of Technology & Infrastructure”
-      </div>
-    </div>
-  );
-};
+// import React from "react";
+// import "./NavbarLogo.css";
+// const NavbarLogo = () => {
+//   return (
+//     <div className="company_logo">
+//       <div className="logo">
+//         <div className="vertical1"></div>
+//         <div className="vertical2"></div>
+//         <div className="vertical3">&#8734;</div>
+//         <div className="vertical1"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical7"></div>
+//         <div className="vertical8"></div>
+//         <div className="vertical9"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical12"></div>
+//         <div className="vertical13"></div>
+//         <div className="vertical14"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical17"></div>
+//         <div className="vertical18"></div>
+//         <div className="vertical19"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical1"></div>
+//         <div className="vertical24"></div>
+//         <div className="vertical1"></div>
+//       </div>
+//       <div className="title">
+//         exicom
+//         <div className="arrows">
+//           <div className="arrow1"></div>
+//           <div className="arrow2"></div>
+//           <div className="arrow3"></div>
+//           <div className="arrow4"></div>
+//         </div>
+//       </div>
+//       <div className="tagline">
+//         A Integrated Bridging of Technology & Infrastructure”
+//       </div>
+//     </div>
+//   );
+// };
 
-export default NavbarLogo;
+// export default NavbarLogo;
