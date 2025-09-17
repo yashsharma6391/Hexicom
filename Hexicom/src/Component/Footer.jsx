efdf539755657e04cd7d357c252ffc7bcd91559f
yashsharma6391 <ys7097569@gmail.com>
2025-09-17 17:08:50 +0530

    Add files via upload

---
+++ Hexicom/src/Component/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+     
+        <div className="footer_text">
+            <p>&copy; 2025 H£XICOM All Rights Reserved</p>
+
+        </div>
+    </div>
+    
+  );
+};
+
+export default Footer;
